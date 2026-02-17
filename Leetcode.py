64c4f8f238effcb214b16ed19270f82aaa30af54
MukeshKumar M <mukeshkumarpandian@gmail.com>
2026-02-17 16:12:42 +0530

    prob 13 is sunday's and prob 14 is monday's and today or tuesday's problem is 15

--- Leetcode.py
+++ Leetcode.py
@@ -434,11 +434,28 @@ class Solution():
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
- #15 problem --                                date: 17-02-2026
+ #15 problem --   Robot retur to origin                             date: 17-02-2026
 
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x=0
+        y=0
 
-
-    
+        for i in moves:
+            if i == "R":
+                x+=1
+            elif i == "L":
+                x-=1
+            elif i == "U":
+                y+=1
+            elif i == "D":
+                y-=1
+        return x == 0 and y ==0
+            
+##print(Solution().judgeCircle("R,L,U,D"))
+##print(Solution().judgeCircle("R,L")) 
+##print(Solution().judgeCircle("R,U,D"))  
+##print(Solution().judgeCircle("R,L,U"))       
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
  #16 problem --                                date: 18-02-2026
