cf1a34473dc448dcd36231dfc24cffbcc2a0045a
MukeshKumar M <mukeshkumarpandian@gmail.com>
2026-03-18 20:32:06 +0530

    day 35 without LC problem  and some inter problems  on class

--- Python_class-uniq/7_Exception_Handling.py
+++ Python_class-uniq/7_Exception_Handling.py
@@ -158,7 +158,7 @@ d=sorted (c, key=lambda x:x[1], reverse=True)
 print(d)
 print(d[1][0])
 '''
-#  op == [24,12,8.6]
+#  op == [24,12,8,6]
 
 # all multiple
 # and divide by input by increacing 1
@@ -173,22 +173,263 @@ for i in x:
     n=m//i
     y.append(n)
 print(y)
+
 '''
 
+# False
 
+##print(0.1+0.2==0.3)
 
+# op -- [3,4,5,1,2]  -- change upto input  index 
+'''
+x=[1,2,3,4,5]
+y=2
+z=x[y:]+x[:y]
+print(z)
+'''
+# change upto iput index to last
+'''
+a=[1,2,3,4,5]
+b=int(input())
 
+for i in range(k):
+    x=a.pop(0)
+    a.append(x)
+print(a)
+'''
+# change input only to last 
 
+'''
+a=[1,2,3,4,5]
+b=int(input())
+j=a[0]
+for i in range(len(a)):
+    if a[i] == b:
+        a.remove(b)
+        a.append(b)
+        break
+print(a)
+'''
 
+##      non repeating string 
+        
 
+'''
+x="aabccc"
+y=list(x)
+for i in y:
+    z=y[len(y):i:-1]
+    if  i in z:
+        y=pop(i)
+print(y)
+'''
+# Another Method
+'''
+x="aabccc"
+def func(a):
+    for i in a:
+        if a.count(i)==1:
+            return i
+print(func(x))
+'''
+# Another Method
+
+'''
+a="aabbccc"
+for i in a:
+    if a.count(i)!=1:
+         print(None)
+         break
+    else:
+        print(i)
+'''
+#  Missing Sequence Numbers -- op -- [4,6]
+
+'''
+x=[1,2,3,5,7]
+y=[]
+z=x[0]
+for i in range(len(x)):
+    if x[i] != z:
+        y.append(z)
+    z=x[i]+1
+print(y)
+'''
+
+# longest substring  -- Works but not for all
+'''
+a="ababcbaac"
+#a="bankaitensazangetsu"
+##b=list(a)
+
+x={1:""}
+j=1
+for i in a:
+    if i not in x[j]:
+        x[j]+=i
+    else:
+        j+=1
+        x[j]=i
+print(x)
+
+##print(x.values())
+##m={1:"abc",2:"ab"}
+##print(m[2])
+
+y=[]
+for i in x.values():
+    if len(i)>len(y):
+        y=i
+print(y)
+'''
 # ____________________________________________________________________________________________________________________________________________________________________________________________
 
 #                                                      Date: 17-03-2026
 
+#longest substring without duplicate values
+'''
+x="bankaitensazangatsu"
+##x="abadbc"
+s=set()
+l=0
+length=0
+
+for r in range(len(x)):
+    while x[r] in s:
+        s.remove(x[l])
+        l+=1
+    s.add(x[r])
+##    if length>r-l+1:
+##        length = length
+##    else:
+##        length = r-l+1
+    length=max(length,r-l+1)
+print(length)
+'''            
+    
+# op -- ["cba","fed"] -- reverse string inside list
+'''
+x=["abc","def"]
+y=[]
+z=""
+for i in x:
+    for j in i:
+        z=j+z
+    y.append(z)
+    z=""
+print(y)
+'''
+
+# Duplicate times diferent Values 
+'''
+a=[1,2,2,1,3,1,3]
+b={}
+c=0
+for i in a:
+    if i not in b:
+        b[i]=1
+    else:
+        b[i]+=1
+print(b)
+d=set(b.values())
+e=list(b)
+if len(d)== len(e):
+    print("True")
+else:
+    print("False")
+'''
+# Same Method but simple for loop
+'''
+a=[1,2,2,1,3,1,3]
+b={}
+c=0
+for i in a:
+    b[i]=a.count(i)
+
+print(b)
+d=set(b.values())
+e=list(b)
+if len(d)== len(e):
+    print("True")
+else:
+    print("False")
+'''
+
+# op -- "abcdabef" removal of sequence of non repeating strings
 
 
 
-# ____________________________________________________________________________________________________________________________________________________________________________________________
+'''
+a="aaabcddabbef"
+b=""
+for i in range(len(a)):
+    if a[i] != a[i-1]:
+        b+=a[i]
+print(b)
+'''
+# Slightly differnece
+
+
+'''
+a="aaabcddabbef"
+b=""
+s=a[0]
+for i in range(1,len(a)):
+    if a[i] != a[i-1]:
+        s+=a[i]
+print(b)
+'''
+
+# op -- [[1,1,1],[2,2],[3,3,3],[4,4]]
+'''
+x=[1,1,1,2,2,3,3,3,4,4]
+y=[]
+z=[x[0]]
+for i in range(1,len(x)):
+    
+    if  x[i] == x[i-1]:
+        z.append(x[i])
+    else:
+        y.append(z)
+        z=[x[i]]
+y.append(z)
+print(y)
+'''    
+# op -- [[1,1,1],[4,4],[2,2],[7],[6],[5]]
+
+x=[1,1,1,2,2,4,4,5,6,7,8]
+y=[]
+z=[x[0]]
+for i in range(1,len(x)):
+    
+    if  x[i] == x[i-1]:
+        z.append(x[i])
+    else:
+        y.append(z)
+        z=[x[i]]
+y.append(z)
+print(y)
+
+    
+##a=x[0]
+a=sorted(y,key=lambda b:(len(b),b[0]) ,reverse=True)
+print(a)
+
+##b=[y[0]]
+####b.insert(0,y[2])
+##
+##for i in range(1,len(y)):
+##    
+##    if len(y[i]) > len(b[i-1]):
+##        b.insert(i,y[i])
+##    elif len(y[i])==len(b[i-1]):
+##        b.insert(i-1,y[i])
+##    else:
+##        b.append(y[i])
+##print(b)
+
+
+### ____________________________________________________________________________________________________________________________________________________________________________________________
 
 #                                                      Date: 18-03-2026
 
