43625224e7124c3f01be687e9f0f291083e33686
MukeshKumar M <mukeshkumarpandian@gmail.com>
2026-04-10 20:10:51 +0530

    Daemon_thread on class

--- Python_class-uniq/9_MultiThreding.py
+++ Python_class-uniq/9_MultiThreding.py
@@ -369,6 +369,272 @@ t2.start()
 
 #                                                                      Date : 10-04-2026
 
+# is_alive() method
+'''
+def func():
+    for i in range(5):
+        print("shikkai")
+def show():
+    for i in range(5):
+        print("bankai")
+
+t1=Thread(target=func)
+t2=Thread(target=show)
+print("Before t1",t1.is_alive())  # it will return False because t1 is not started yet
+print("Before t2",t2.is_alive())  # it will return False because t2 is not started yet
+
+t1.start()
+t2.start()
+print("After t1",t1.is_alive())  # it will return True because t1 is started
+print("After t2",t2.is_alive())  # it will return True because t2 is started
+'''
+# active_count() method returns the number of active threads in the current thread's thread group. It includes the main thread and all the threads that are currently running.
+#print("Active threads",active_count())  # it will return 3 because main thread, t1 and t2 are active
+
+# active_count() method with time sleep() method 
+# main thread will be active until all the threads are completed
+'''
+def func():
+    for i in range(5):
+        print("shikkai")
+        time.sleep(1)
+def show():
+    for i in range(5):
+        print("bankai")
+
+t1=Thread(target=func)
+t2=Thread(target=show)
+
+print("Before t1",t1.is_alive()) 
+print("Before t2",t2.is_alive())  
+
+t1.start()
+t2.start()
+
+print("Active threads",active_count())
+'''
+
+# enumerate() method returns a list of all the active threads in the current thread's thread group. It includes the main thread and all the threads that are currently running.
+'''
+def func():
+    for i in range(5):
+        print("shikkai")
+        time.sleep(1)
+def show():
+    for i in range(5):
+        print("bankai")
+        time.sleep(1)
+
+t1=Thread(target=func)
+t2=Thread(target=show)
+print("Before t1",t1.is_alive()) 
+print("Before t2",t2.is_alive())
+t1.start()
+t2.start()
+print("Active threads",active_count())
+print("Enumerate threads",enumerate())  # it will return a list of all the active threads in the current thread's thread group
+'''
+
+# process ID -- os.getpid()
+'''
+import os
+from threading import *
+
+def func():
+    print("process ID of func",os.getpid())
+    print("shikkai")
+def display():
+    print("process ID of display",os.getpid())
+    print("bankai")
+
+t1=Thread(target=func)
+t2=Thread(target=display)
+t1.start()
+t2.start()
+print("process ID of main thread",os.getpid())  # it will return the process ID of the main thread
+'''
+
+
+# thread ID -- t1.ident
+'''
+import os
+from threading import *
+
+def func():
+    print("thread ID of func",current_thread().ident)
+    print("shikkai")
+def display():
+    print("thread ID of display",current_thread().ident)
+    print("bankai")
+t1=Thread(target=func)
+t2=Thread(target=display)
+t1.start()
+t2.start()
+print("thread ID of main thread",current_thread().ident)  # it will return the thread ID of the main thread
+'''
+
+# daemon thread -- a thread that runs in the background and does not block the main thread from exiting. It is used for tasks that are not critical to the main thread and can be safely ignored if the main thread exits.
+
+
+# Daemon threads are running background
+# Daemon threads are used to provide the supports for non daemon thread
+# Daemon threads are running continuously in memory
+# Garbage collecter is the best example of daemon thread
+# Garbage collector is used to delete useless objects from the memory at the time of program execution
+# By using daemon property we can set a thread as daemon thread, as well as by using this property we can check s thread is daemon thread or not
+# Remember main thread is always non-deamon and we can't change main thread as daemon thread
+# Once a thread started then we can't change its nature , so main thread started by pvm , so its not in our hand, that's why we can't change the default nature of main thread.
+# Except main thread, remaining all threads nature depends nature depends on its parent, if the parent is daemon, then child also daemon , and vice-versa.
+# we can change the nature of other thread except main thread.
+# Daemon thread are widely used to maintain log record, grammer checker, scrap data from web in the background.
+
+'''
+from threading import *
+import time
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5):
+        print("shikkai")
+def display():
+    print("display()",current_thread().daemon)
+    for i in range(5):
+        print("bankai")
+t1=Thread(target=func)
+t2=Thread(target=display)
+t1.start()
+t2.start()
+'''
+
+# daemon thread with time sleep() method -- if the main thread exits then the daemon thread will also exit even if it is not completed
+# in real time like bus ticket booking website, we can use daemon thread to maintain log record, if the main thread exits then the daemon thread will also exit even if it is not completed, so it will save the memory and also it will not create any problem because log record is not critical to the main thread.
+'''
+#
+from threading import *
+import time
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5):
+        time.sleep(0.5)
+        print("shikkai")
+
+t1=Thread(target=func,daemon=True) 
+t1.start()
+time.sleep(1)
+print("main thread is exiting")  # main thread is exiting after 1 second, so the daemon thread will also exit even if it is not completed
+'''
+#
+'''
+from threading import *
+import time
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5):
+        time.sleep(0.5)
+        print("shikkai")
+        
+def display():
+    print("display()",current_thread().daemon)
+    for i in range(5):
+        time.sleep(1)
+        print("bankai")
+        
+t1=Thread(target=func)
+t2=Thread(target=display,daemon=True)
+t1.start()
+t2.start()
+'''
+
+
+
+'''
+from threading import *
+import time
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5):
+        print("shikkai")
+def display():
+    print("display()",current_thread().daemon)
+    for i in range(5):
+        print("bankai")
+        time.sleep(1)
+t1=Thread(target=func)
+t2=Thread(target=display)
+t2.daemon=True  # it will make t2 a daemon thread
+# t2.setDaemon(True)  # it will also make t2 a daemon thread
+# t2=Thread(target=diplay,daemon=True) # it will alo make t2 a daemon thread)
+t1.start()
+t2.start()
+'''
+# daemon thread inside a normal thread -- it will automatically crete daemon thread because its parent is daemon thread
+'''
+from threading import *
+import time
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5):
+        print("shikkai")
+    t2=Thread(target=display)
+    t2.start()
+    print("t2 is daemon",t2.daemon)  
+def display():
+    print("display()",current_thread().daemon)
+    for i in range(5):
+        time.sleep(1)
+        print("bankai")
+t1=Thread(target=func,daemon=True)
+t1.start()
+time.sleep(2)
+print("main thread is exiting")
+'''
+# daemon thread inside a non daemon thread -- using daemon=False but its parent is daemon thread
+'''
+from threading import *
+import time
+
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5): 
+        print("shikkai")
+    t2=Thread(target=display,daemon=False)
+    t2.start()
+
+def display():
+    print("display()",current_thread().daemon)
+    for i in range(5):
+        print("bankai")
+        time.sleep(1)
+        
+t1=Thread(target=func,daemon=True)
+t1.start()
+time.sleep(2)
+print("main thread is exiting")  
+'''
+
+# thread inside a daemon thread -- using daemon=True but its parent is daemon thread
+'''
+def func():
+    print("func()",current_thread().daemon)
+    for i in range(5):
+        print("shikkai")
+    t2=Thread(target=display,daemon=True)  # it will return the current thread object
+    t2.start()
+def display():
+    print("display()",current_thread().daemon)
+    for i in range(5):
+        time.sleep(1)
+        print("bankai")
+t1=Thread(target=func)
+t1.start()
+time.sleep(1)
+print("main thread is exiting")
+'''
+
+
+
+
+
+
 
 #____________________________________________________________________________________________________________________________________
 
